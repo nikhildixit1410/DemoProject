d3ed0102056f4e7bbf5ca7af2b944756980e63ad
nikhildixit1410 <nikhildixit1410@gmail.com>
2025-04-07 18:00:05 +0530

    i am the best

--- src/A.java
+++ src/A.java
@@ -1,9 +1,11 @@
 
 public class A {
-
+public void add ()
+{
+	System.out.println("ssquareit");
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+     System.out.println("hello");
 	}
 
 }
